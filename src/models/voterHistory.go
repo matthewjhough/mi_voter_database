3c94076fb7e7c5c6367294d52dd1f8eabc398bcb
Nathan Mentley <nathanmentley@gmail.com>
2018-08-09 10:44:25 -0400

    Added bsd notice to the top of each source file. I doubt this really matters, but it helps me sleep better at night I suppose.

--- src/models/voterHistory.go
+++ src/models/voterHistory.go
@@ -1,3 +1,13 @@
+/* mi_voter_database
+ * 
+ * Copyright (C) 2018 Nathan Mentley - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the BSD license.
+ *
+ * You should have received a copy of the BSD license with
+ * this file. If not, please visit: https://github.com/nathanmentley/mi_voter_database
+ */
+
 package models
 
 type VoterHistory struct {
